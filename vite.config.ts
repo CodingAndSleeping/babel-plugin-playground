cdc4b593584471c1d91942f88d123fadf513d727
lzt <1162300237@qq.com>
2025-03-27 00:08:51 +0800

    feat: 🚀 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/babel-plugin-playground/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
